03fc778f68480aaea69e85ae602061bc6f1217f8
David Smith <dnsmith124@gmail.com>
2022-06-10 14:21:53 -0500

    styling updates to better reflect spotify's ui

--- src/components/spotify-app/Spotify/Spotify.js
+++ src/components/spotify-app/Spotify/Spotify.js
@@ -65,11 +65,9 @@ const Spotify = () => {
 
   return (
     <div className="spotify-window">
-      <div className="container-xxl">
-        {token 
-          ? <SpotifyApp spotify={spotify} /> 
-          : <SpotifyLogin loginUrl={loginUrl} />}
-      </div>
+      {token 
+        ? <SpotifyApp spotify={spotify} /> 
+        : <SpotifyLogin loginUrl={loginUrl} />}
     </div>
   );
 }
